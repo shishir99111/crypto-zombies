7e44c7e0582bff4295470f2b414de527955b320e
shishir99111 <shishir99111@gmail.com>
2018-04-20 11:10:59 +0530

    payable contract for premium level up

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -4,9 +4,24 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  function withdraw() external onlyOwner {
+    owner.transfer(this.balance);
+  }
+
+  function setLevelUpFee(uint _fee) external onlyOwner {
+    levelUpFee = _fee;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
